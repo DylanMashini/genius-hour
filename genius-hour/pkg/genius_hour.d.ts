429fe7acbbdc61614fe9c40f535c0b0f0fbedf77
Dylan Mashini <dylanmashini123@gmail.com>
2025-05-07 12:05:00 -0400

    Trying to do frontend

---
+++ genius-hour/pkg/genius_hour.d.ts
@@ -0,0 +1,6 @@
+/* tslint:disable */
+/* eslint-disable */
+export function ensure_model_loaded(): void;
+export function predict_mnist(image_data: Float32Array): Float32Array;
+export function get_model_input_size(): number;
+export function get_model_output_size(): number;
---
+++ genius-hour/pkg/genius_hour.js
@@ -0,0 +1,9 @@
+import * as wasm from "./genius_hour_bg.wasm";
+export * from "./genius_hour_bg.js";
+import { __wbg_set_wasm } from "./genius_hour_bg.js";
+__wbg_set_wasm(wasm);
+wasm.__wbindgen_start();
+
+export function init() {
+  wasm.__wbindgen_start();
+}
