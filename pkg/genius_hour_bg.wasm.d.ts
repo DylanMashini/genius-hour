98e85b2b3d526d1be1c6ec1150d0aa618d3a1abd
DylanMashini <54782808+DylanMashini@users.noreply.github.com>
2025-05-07 15:42:32 +0000

    Add WASM Build (Won't build on m

---
+++ pkg/genius_hour_bg.wasm.d.ts
@@ -0,0 +1,12 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const ensure_model_loaded: () => [number, number];
+export const predict_mnist: (a: number, b: number) => [number, number, number, number];
+export const get_model_input_size: () => number;
+export const get_model_output_size: () => number;
+export const __wbindgen_export_0: WebAssembly.Table;
+export const __externref_table_dealloc: (a: number) => void;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_start: () => void;
